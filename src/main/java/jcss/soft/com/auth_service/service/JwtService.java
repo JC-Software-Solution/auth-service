3cefb4d4c85aa5bc1257fa2d897ddb9ccf995473
Jovanie Cabatuan <jhovaniecabatuan@gmail.com>
2026-06-27 16:21:16 +0800

    private key

--- src/main/java/jcss/soft/com/auth_service/service/JwtService.java
+++ src/main/java/jcss/soft/com/auth_service/service/JwtService.java
@@ -11,21 +11,24 @@ import jcss.soft.com.auth_service.repository.UserRepository;
 import jcss.soft.com.auth_service.dtos.response.ResponseObject;
 import jcss.soft.com.auth_service.spel.IUserSpel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.*;
 
 import static jcss.soft.com.auth_service.constants.Constants.*;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class JwtService {
     private final UserRepository userRepository;
     private final ObjectMapper mapper;
@@ -49,20 +52,25 @@ public class JwtService {
     }
 
     public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) {
-        return Jwts.builder()
-                .setClaims(extractClaims)
-                .setSubject(userDetails.getUsername())
-                .issuer("auth-service")
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSignInKey(), SignatureAlgorithm.RS256).compact();
+        try {
+            return Jwts.builder()
+                    .setClaims(extractClaims)
+                    .setSubject(userDetails.getUsername())
+                    .issuer("auth-service")
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                    .signWith(getPrivateKey(), SignatureAlgorithm.RS256).compact();
+        }catch (Exception e) {
+            log.debug("Key error", e);
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     private Claims extractAllClaims(String token) {
         try {
             return Jwts
                     .parser()
-                    .setSigningKey(getSignInKey())
+                    .setSigningKey(getPrivateKey())
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
@@ -72,9 +80,19 @@ public class JwtService {
         }
     }
 
-    private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
+//    private Key getSignInKey() {
+//        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+//        return Keys.hmacShaKeyFor(keyBytes);
+//    }
+
+    private PrivateKey getPrivateKey() throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(SECRET_KEY);
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+        return keyFactory.generatePrivate(spec);
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
